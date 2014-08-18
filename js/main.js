ad6de3601e0cd641c34be528c2e4b2dd474e6574
Edgar Albalate <dev.gardo@gmail.com>
2014-08-18 21:52:29 +0000

    Muchos cambios en la estructura del header

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(document, window){
+  function analizeScroll(){
+    if(location.pathname.length>1) return $('body').addClass('shortHead');
+    if($(window).scrollTop()<40)
+      $('body').removeClass('shortHead');
+    else
+      $('body').addClass('shortHead');
+  }
+  $(document).ready(function(){
+    $(window).on('scroll', analizeScroll);
+    analizeScroll();
+  });
+  
+})(document, window);
